3ba053cc726c52d587f3784e62b5d6b197f4bc9e
Jowita Stachowiak <crazylittlepolka@gmail.com>
2018-08-03 16:43:14 +0200

    Books without cover can be searched

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,7 @@ class Book extends Component {
       return (                        
                   <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: `url("${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''}")` }}></div>
                             <div className="book-shelf-changer">
                               <select
                                 value={this.props.bookShelf.id}
